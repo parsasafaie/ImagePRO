ccab340980a107b3eb01539377c70719145e86f6
parsasafaie <parsasafaie.2568@proton.me>
2025-07-11 17:52:34 +0330

    add unsharp_making method for sharpening

--- IMAGE_FILTERING/sharpening.py
+++ IMAGE_FILTERING/sharpening.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from blur import average_blur
 
 def laplacian_filter(laplacian_coefficient=3, image_path=None, np_image=None, result_path=None):
     if image_path:
@@ -10,6 +11,21 @@ def laplacian_filter(laplacian_coefficient=3, image_path=None, np_image=None, re
 
     sharpen_image = np_image + laplacian_coefficient * laplacian
     sharpen_image = np.uint8(np.clip(sharpen_image, 0, 255))
+
+    if result_path:
+        cv2.imwrite(result_path, sharpen_image)
+    else:
+        return sharpen_image
+
+
+def unsharp_masking(coefficient=1, image_path=None, np_image=None, result_path=None):
+    if image_path:
+        np_image = cv2.imread(image_path)
+
+    blur_image = average_blur(np_image=np_image)
+
+    mask = cv2.subtract(np_image, blur_image)
+    sharpen_image = cv2.addWeighted(np_image, 1 + coefficient, mask, -coefficient, 0)
 
     if result_path:
         cv2.imwrite(result_path, sharpen_image)
