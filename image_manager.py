9e97899270b9ca633b5763e3262932e6328dcd97
parsasafaie <parsasafaie.2568@proton.me>
2025-07-11 18:02:26 +0330

    add image manager and some utility functions

---
+++ image_manager.py
@@ -0,0 +1,12 @@
+import cv2
+
+def input_manager(image_path=None, np_image=None):
+    np_image = cv2.imread(image_path) if image_path else np_image
+    return np_image
+
+def output_manger(np_image, result_path=None):
+    if result_path:
+        cv2.imwrite(result_path, np_image)
+        return f"File saved at {result_path}"
+    else:
+        return np_image
