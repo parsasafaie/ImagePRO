07cc069556da5c9dc41de3aac35bf55ba8e76665
parsasafaie <parsasafaie.2568@proton.me>
2025-07-11 11:27:40 +0330

    add clahe method for contrast

---
+++ PRE_PROCESSING/contrast.py
@@ -0,0 +1,16 @@
+import cv2
+from grayscale import grayscale
+
+def enhance_contrast_clahe(clipLimit=2.0, tileGridSize=(8, 8), image_path=None, np_image=None, result_path=None):
+    if image_path:
+        np_image = cv2.imread(image_path,0)
+    else:
+        np_image = grayscale(np_image=np_image)
+
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=tileGridSize)
+    enhanced_image = clahe.apply(np_image)
+
+    if result_path:
+        cv2.imwrite(result_path, enhanced_image)
+    else:
+        return enhanced_image
