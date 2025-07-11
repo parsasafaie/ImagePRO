6998d7c28653dd9a266d912e57abfb975e21a142
parsasafaie <parsasafaie.2568@proton.me>
2025-07-11 11:28:39 +0330

    add GHE method for contrast

--- PRE_PROCESSING/contrast.py
+++ PRE_PROCESSING/contrast.py
@@ -14,3 +14,17 @@ def enhance_contrast_clahe(clipLimit=2.0, tileGridSize=(8, 8), image_path=None, 
         cv2.imwrite(result_path, enhanced_image)
     else:
         return enhanced_image
+
+
+def enhance_contrast_GHE(image_path=None, np_image=None, result_path=None):
+    if image_path:
+        np_image = cv2.imread(image_path,0)
+    else:
+        np_image = grayscale(np_image=np_image)
+
+    enhanced_image = cv2.equalizeHist(np_image)
+
+    if result_path:
+        cv2.imwrite(result_path, enhanced_image)
+    else:
+        return enhanced_image
