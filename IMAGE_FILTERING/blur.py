248d4e6ad425ce00e38c8ce80cbf49974a0e4bc1
parsasafaie <parsasafaie.2568@proton.me>
2025-07-11 14:32:36 +0330

    add median blur method for blur

--- IMAGE_FILTERING/blur.py
+++ IMAGE_FILTERING/blur.py
@@ -22,3 +22,15 @@ def gaussian_blur(kernel_size=(5, 5), image_path=None, np_image=None, result_pat
         cv2.imwrite(result_path, blur_image)
     else:
         return blur_image
+    
+
+def median_blur(filter_size=5, image_path=None, np_image=None, result_path=None):
+    if image_path:
+        np_image = cv2.imread(image_path)
+
+    blur_image = cv2.medianBlur(np_image, filter_size)
+
+    if result_path:
+        cv2.imwrite(result_path, blur_image)
+    else:
+        return blur_image
