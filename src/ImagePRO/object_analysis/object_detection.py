228118b66cb1f6fada28fea561c0f9b99747346d
parsasafaie <parsasafaie.2568@proton.me>
2025-08-13 12:11:04 +0330

    add csv output feature to detect_objects function

--- src/ImagePRO/object_analysis/object_detection.py
+++ src/ImagePRO/object_analysis/object_detection.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import sys
 from pathlib import Path
+import cv2
 
 # Add parent directory to path for custom module imports
 parent_dir = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ sys.path.append(str(parent_dir))
 
 from utils.io_handler import IOHandler
 
-def detect_objects(src_image_path=None, src_np_image=None, output_image_path=None, show_result=False):
+def detect_objects(src_image_path=None, src_np_image=None, output_image_path=None, output_csv_path=None, show_result=False):
     model = YOLO("yolo11n.pt") 
     frame = IOHandler.load_image(image_path=src_image_path, np_image=src_np_image)
     result = model(frame)[0]
@@ -17,5 +18,7 @@ def detect_objects(src_image_path=None, src_np_image=None, output_image_path=Non
         result.show()
     if output_image_path:
         IOHandler.save_image(np_image=result.plot(), result_path=output_image_path)
-    
-    return result
+    if output_csv_path:
+        boxes = result.boxes
+        lines = [[box.cls, box.xyxyn] for box in boxes]
+        IOHandler.save_csv(data=lines, result_path=output_csv_path)
