0b29e00efb377e07553c9a83bbbc0cc6cb53d7c3
parsasafaie <parsasafaie.2568@proton.me>
2025-08-13 12:48:49 +0330

    fix csv bug in detect_objects

--- src/ImagePRO/object_analysis/object_detection.py
+++ src/ImagePRO/object_analysis/object_detection.py
@@ -28,19 +28,20 @@ def detect_objects(model=None, accuracy_level=1, src_image_path=None, src_np_ima
 
     frame = IOHandler.load_image(image_path=src_image_path, np_image=src_np_image)
     result = model(frame)[0]
+    boxes = result.boxes
+    
+    lines = []
+    for box in boxes:
+        box_class = int(box.cls)
+        conf  = float(box.conf)
+        x1, y1, x2, y2 = [float(c) for c in box.xyxyn.squeeze().tolist()]
+        lines.append([box_class,[x1, y1, x2, y2], conf])
 
     if show_result:
         result.show()
     if output_image_path:
         IOHandler.save_image(np_image=result.plot(), result_path=output_image_path)
     if output_csv_path:
-        boxes = result.boxes
-        lines = []
-        for box in boxes:
-            box_class = int(box.cls)
-            conf  = float(box.conf)
-            x1, y1, x2, y2 = [float(c) for c in box.xyxyn.squeeze().tolist()]
-            lines.append([box_class,[x1, y1, x2, y2], conf])
         IOHandler.save_csv(data=lines, result_path=output_csv_path)
 
     return result.plot(), lines
